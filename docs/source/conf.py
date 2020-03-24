42b67dadc988fc1a5fbd644e5d3ed83735b69229
anmearge <ama.gehlen@gmail.com>
2020-03-24 22:21:35 +0100

    Change documentation theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import yummy_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -29,8 +30,11 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+#extensions = []
 
+extensions = [
+    "yummy_sphinx_theme",
+]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
@@ -45,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "yummy_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
