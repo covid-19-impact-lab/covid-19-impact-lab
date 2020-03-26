aa4f101c79ab4c8f8e3fb6972b83653f3fffa8cf
Annica Gehlen <39128048+amageh@users.noreply.github.com>
2020-03-26 17:39:37 +0100

    Change documentation theme. (#5)
    * Use yummy theme, OSE logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = "CoViD-19 Impact Lab"
@@ -29,6 +28,7 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,10 +44,22 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = "alabaster"
-
+html_theme_path = ["_themes", ]
+html_theme = "yummy_sphinx_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+
+html_theme_options = {
+    'navs': {
+        'GitHub': 'https://github.com/covid-19-impact-lab'
+    },
+    'service': 'GitHub',
+    'user': 'covid-19-impact-lab',
+    'repo': 'covid-19-impact-lab',
+    'navbar_icon': 'spin fa-book',
+}
+
+html_logo = '_static/images/ose_logo.ico'
